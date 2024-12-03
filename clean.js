c0383208be7735476ee6ca12150d78212332db3d
k-one-o-two <kirill.s@skiff.com>
2024-12-03 12:00:22 +0200

    date

--- clean.js
+++ clean.js
@@ -4,6 +4,9 @@ const db = new locallydb('./mydb');
 const bestOf24Array = db.collection('bestOf24Array');
 
 const run = () => {
+  collection.remove(0);
+  collection.remove(0);
+
   const entries = bestOf24Array.items;
 
   console.log(entries);
