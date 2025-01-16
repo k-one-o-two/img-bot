f6f6ffce3de750c98c4d3322ee3e444ac2a46d03
k-one-o-two <kirill.s@skiff.com>
2025-01-16 19:57:11 +0200

    vertical

--- app.js
+++ app.js
@@ -147,52 +147,106 @@ const messWithImages = async () => {
   const image = await Jimp.read('output.jpg');
   const { width, height } = image.bitmap;
 
+  const isVertical = height > width;
+
   const stamp = await Jimp.read(`stamps/${randomIntFromInterval(1, 5)}.png`);
 
-  stamp.resize({ h: height / 5 });
-  const { width: stampWidth, height: stampHeight } = stamp.bitmap;
+  if (isVertical) {
+    stamp.resize({ w: width / 5 });
+    const { width: stampWidth, height: stampHeight } = stamp.bitmap;
 
-  // apply borders
-  const borderH = new Jimp({ width, height: border, color: 0xffffffff });
-  image.composite(borderH, 0, 0);
-  image.composite(borderH, 0, height - border);
+    // apply borders
+    const borderH = new Jimp({ width, height: border, color: 0xffffffff });
+    image.composite(borderH, 0, 0);
+    // image.composite(borderH, 0, height - border);
 
-  const borderR = new Jimp({ width: border, height, color: 0xffffffff });
-  image.composite(borderR, width - border, 0);
+    const borderV = new Jimp({ width: border, height, color: 0xffffffff });
+    image.composite(borderV, width - border, 0);
+    image.composite(borderV, 0, 0);
 
-  const borderL = new Jimp({ width: border * 4, height, color: 0xffffffff });
-  image.composite(borderL, 0, 0);
+    const borderB = new Jimp({ width, height: border * 4, color: 0xffffffff });
+    image.composite(borderB, 0, height - border * 4);
 
-  const overlay = new Jimp({ width, height, color: 0x000000ff });
-  overlay.opacity(0.1);
+    const overlay = new Jimp({
+      width,
+      height: height - border * 3,
+      color: 0x000000ff,
+    });
+    overlay.opacity(0.1);
 
-  image.composite(overlay, border * 3, 0);
+    image.composite(overlay, 0, 0);
 
-  const stampBg = new Jimp({
-    width: stampWidth,
-    height: stampHeight,
-    color: 0xffffffff,
-  });
+    const stampBg = new Jimp({
+      width: stampWidth,
+      height: stampHeight,
+      color: 0xffffffff,
+    });
 
-  stampBg.opacity(0.1);
+    stampBg.opacity(0.1);
 
-  stamp.composite(stampBg, 0, 0);
-  stamp.rotate(stampRotate);
+    stamp.composite(stampBg, 0, 0);
+    stamp.rotate(stampRotate);
 
-  image.composite(stamp, width - stampWidth - stampXOffset, stampYOffset);
+    image.composite(stamp, width - stampWidth - stampXOffset, stampYOffset);
 
-  const font = await loadFont('./font/18.fnt');
+    const font = await loadFont('./font/18.fnt');
 
-  image.rotate(90);
+    // image.rotate(90);
 
-  image.print({
-    font,
-    x: border,
-    y: width - 50,
-    text: `postikortti suomesta, ${months[monthIndex]}`,
-  });
+    image.print({
+      font,
+      x: border,
+      y: height - 50,
+      text: `postikortti suomesta, ${months[monthIndex]}`,
+    });
 
-  image.rotate(-90);
+    // image.rotate(-90);
+  } else {
+    stamp.resize({ h: height / 5 });
+    const { width: stampWidth, height: stampHeight } = stamp.bitmap;
+
+    // apply borders
+    const borderH = new Jimp({ width, height: border, color: 0xffffffff });
+    image.composite(borderH, 0, 0);
+    image.composite(borderH, 0, height - border);
+
+    const borderR = new Jimp({ width: border, height, color: 0xffffffff });
+    image.composite(borderR, width - border, 0);
+
+    const borderL = new Jimp({ width: border * 4, height, color: 0xffffffff });
+    image.composite(borderL, 0, 0);
+
+    const overlay = new Jimp({ width, height, color: 0x000000ff });
+    overlay.opacity(0.1);
+
+    image.composite(overlay, border * 3, 0);
+
+    const stampBg = new Jimp({
+      width: stampWidth,
+      height: stampHeight,
+      color: 0xffffffff,
+    });
+
+    stampBg.opacity(0.1);
+
+    stamp.composite(stampBg, 0, 0);
+    stamp.rotate(stampRotate);
+
+    image.composite(stamp, width - stampWidth - stampXOffset, stampYOffset);
+
+    const font = await loadFont('./font/18.fnt');
+
+    image.rotate(90);
+
+    image.print({
+      font,
+      x: border,
+      y: width - 50,
+      text: `postikortti suomesta, ${months[monthIndex]}`,
+    });
+
+    image.rotate(-90);
+  }
 
   await image.write('output_stamp.jpg');
 };
