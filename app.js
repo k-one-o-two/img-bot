92228181476fd2186358168a44497365ba2587d8
k-one-o-two <kirill.s@skiff.com>
2025-06-26 20:58:44 +0300

    square

--- app.js
+++ app.js
@@ -740,6 +740,7 @@ function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+console.info("starting...");
 const bot = createBot();
 setupBotEvents();
 
